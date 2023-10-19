8130347f5125930dd8ad68a4c6cb6059892729b6
yg <eng.yg@yahoo.com>
2023-10-19 10:58:23 +0800

    2.11: The Phonebook Step6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Content = (props) => <div>{props.person.name} {props.person.number}</div>
 const Filter = (props) => <div>filter shown with <input onChange={props.handle} value={props.value} /></div>
@@ -20,12 +21,11 @@ const PersonForm = (props) => {
 const Persons = (props) => props.persons.filter(x => props.regexNameFilter.test(x.name)).map(person => <Content person={person} key={person.id} />)
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+  useEffect(()=>{
+    axios.get("http://localhost:3001/persons")
+    .then(response => setPersons(response.data))
+  },[])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameFilter, setNameFilter] = useState('')
