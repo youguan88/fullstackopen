5989c20886c0d7300a0335c3dcc68f33acc26eef
yg <eng.yg@yahoo.com>
2023-10-22 22:11:39 +0800

    2.19*: Data for countries, step2

--- part2/dataforcountries/src/App.jsx
+++ part2/dataforcountries/src/App.jsx
@@ -1,58 +1,70 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './app.css'
 import languageAPI from './services/language'
 
-let countries = []
-languageAPI.getAll().then(response => countries = response.data)
-
-function App() {
-  const [countryName, setCountryName] = useState('')
-
-  const handleCountryName = (event) =>{
-    setCountryName(event.target.value)
-  }
-  const Country = ({filteredCountries}) => {
-    if (filteredCountries.length == 1)
-    {
-      return (
-        filteredCountries.map(country => <Content key={country.name.common} country={country}/>)
-      )
-    }
-    else if (filteredCountries.length > 10)
-    {
-      return <div>Too many matches, specify another filter</div>
-    }
-    else
-    {
-      return (
-        filteredCountries.map(country => <div>{country.name.common}</div>)
-      )
-    }
-  }
-  const Content = ({country}) => {
-    let languages = Object.values(country.languages)
+const Country = ({ filteredCountries, handleShowButton }) => {
+  if (filteredCountries.length == 1) {
     return (
-      <>
+      filteredCountries.map(country => <Content key={country.name.common} country={country} />)
+    )
+  }
+  else if (filteredCountries.length > 10) {
+    return <div>Too many matches, specify another filter</div>
+  }
+  else {
+    return (
+      filteredCountries.map(country => {
+        const countryName = country.name.common
+        return (
+          <div key={countryName}>
+            {countryName}
+            <ShowButton handleShowButton={() => handleShowButton(countryName)} />
+          </div>
+        )
+      })
+    )
+  }
+}
+
+const Content = ({ country }) => {
+  let languages = Object.values(country.languages)
+  return (
+    <>
       <h1>{country.name.common}</h1>
       <div>capital {country.capital}</div>
       <div>area {country.area}</div>
       <div className='language'><b>languages:</b></div>
       <ul>
-        {languages.map((language)=><li key={language}>{language}</li>)}
+        {languages.map((language) => <li key={language}>{language}</li>)}
       </ul>
       <img src={country.flags.png} />
-      </>
-    )
+    </>
+  )
+}
+
+const ShowButton = ({ handleShowButton }) => {
+  return (<button className="showButton" onClick={handleShowButton}>show</button>)
+}
+
+function App() {
+  const [countries, setCountries] = useState([])
+  const [countryName, setCountryName] = useState('')
+  useEffect(() => {
+    languageAPI.getAll().then(response => setCountries(response.data))
+  }, [])
+  const handleCountryName = (event) => {
+    setCountryName(event.target.value)
+  }
+  const handleShowButton = (countryName) => {
+    setCountryName(countryName)
   }
   const regexNameFilter = RegExp(countryName, 'i')
 
   return (
     <>
-      find countries <input text={countryName} onChange={handleCountryName} />
+      find countries <input value={countryName} onChange={handleCountryName} />
       <div>
-        <div>
-          <Country filteredCountries={countries.filter(country => regexNameFilter.test(country.name.common) & countryName != '')}/>
-        </div>
+        <Country filteredCountries={countries.filter(country => regexNameFilter.test(country.name.common) & countryName != '')} handleShowButton={handleShowButton} />
       </div>
     </>
   )
