a34907933b58b935482bcbc2ff2458429fef2621
yg <eng.yg@yahoo.com>
2023-10-29 09:47:36 +0800

    4.5*: helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,19 @@
 const dummy = (blogs) => {
     return 1
   }
+
+  const totalLikes = (blogs) => {
+    return blogs.map(x=>x.likes).reduce((accumulator, currentValue) => {return accumulator + currentValue},0)
+  }
+
+  const favouriteBlog = (blogs) => {
+    return blogs
+    .map(({title, author, likes}) => ({title, author, likes}))
+    .reduce((accumulator, currentValue) => {return currentValue.likes >= accumulator.likes ? currentValue : accumulator}, {likes:0})
+  }
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favouriteBlog
   }
