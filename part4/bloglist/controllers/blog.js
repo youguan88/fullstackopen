0254b60faa37e1d67c99444647a55b65313e18b4
yg <eng.yg@yahoo.com>
2023-10-31 09:49:40 +0800

    4.8: Blog list tests, step1

--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -1,12 +1,9 @@
 const Blog = require('../models/blog')
 const blogsRouter = require('express').Router()
 
-blogsRouter.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogsRouter.get('/api/blogs', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
 blogsRouter.post('/api/blogs', (request, response) => {
--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = process.env.NODE_ENV === 'test' 
+? process.env.TEST_MONGODB_URI 
+: process.env.MONGODB_URI
+
 const PORT = 3003
 
 module.exports = {
