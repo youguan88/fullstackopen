a03ad66e08b831dba3c0e63111b3a60bd9eef268
yg <eng.yg@yahoo.com>
2023-11-02 10:12:33 +0800

    4.18: bloglist expansion, step6

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blog')
 const usersRouter = require('./controllers/user')
+const loginRouter = require('./controllers/login')
 const config = require('./utils/config')
 require('express-async-errors')
 
@@ -13,5 +14,6 @@ app.use(cors())
 app.use(express.json())
 app.use(blogsRouter)
 app.use('/api/users/', usersRouter)
+app.use('/api/login/', loginRouter)
 
 module.exports = app
