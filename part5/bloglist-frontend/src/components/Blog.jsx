68841bcbe650cfcbb7cf5cd92a6cf9b2efe3fc7c
yg <eng.yg@yahoo.com>
2023-11-07 11:59:00 +0800

    5.7 Blog list frontend, step7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
 
+const Blog = ({ blog }) => {
+  const [view, setView] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const detailsStyle = {
+    display : view ? '' : 'none'
+  }
+
+  const buttonLabel = view ? 'hide' : 'view'
+  const user = blog.user ? blog.user.name : null
+  const toggleVisibility = () => setView(!view)
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>{buttonLabel}</button>
+      </div>
+      <div style={detailsStyle}>
+        <div>{blog.url}</div>
+        <div>
+          {blog.likes}
+          <button>like</button>
+        </div>
+        <div>{user}</div>
+      </div>
+    </div>
+  )
+}
 export default Blog
