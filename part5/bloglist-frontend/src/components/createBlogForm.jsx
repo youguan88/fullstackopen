e46e45ceb3811fe44ba72f7bdf0ea6e19b90238e
yg <eng.yg@yahoo.com>
2023-11-08 19:23:08 +0800

    5.16: Blog list tests, step4

--- part5/bloglist-frontend/src/components/createBlogForm.jsx
+++ part5/bloglist-frontend/src/components/createBlogForm.jsx
@@ -19,9 +19,9 @@ const CreateBlogForm = ({ handleCreateBlog }) => {
     <div>
       <h2>create new</h2>
       <form onSubmit={createBlog}>
-        <div>title:<input type="text" value={title} name="title" onChange={({ target }) => setTitle(target.value)} /></div>
-        <div>author:<input type="text" value={author} name="author" onChange={({ target }) => setAuthor(target.value)} /></div>
-        <div>url:<input type="text" value={url} name="url" onChange={({ target }) => setUrl(target.value)} /></div>
+        <div>title:<input id="title" type="text" value={title} name="title" onChange={({ target }) => setTitle(target.value)} /></div>
+        <div>author:<input id="author" type="text" value={author} name="author" onChange={({ target }) => setAuthor(target.value)} /></div>
+        <div>url:<input id="url" type="text" value={url} name="url" onChange={({ target }) => setUrl(target.value)} /></div>
         <button type="submit">create</button>
       </form>
     </div>
