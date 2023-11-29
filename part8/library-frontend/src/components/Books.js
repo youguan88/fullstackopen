8da2df784f51ee396618f23ed7d80b52d6c9d263
yg <eng.yg@yahoo.com>
2023-11-29 10:20:17 +0800

    8.22 Up-to-date cache and book recommendations

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -4,25 +4,28 @@ import { useState } from "react"
 import BookTable from "./BookTable"
 
 const Books = (props) => {
-  const [genreFilter, setGenreFilter] = useState('')
-  const result = useQuery(All_Books, {
+  const [genreFilter, setGenreFilter] = useState('all genres')
+  const  { loading, data, refetch } = useQuery(All_Books, {
     variables: { genre: genreFilter === "all genres" ? '' : genreFilter }
   })
   if (!props.show) {
     return null
   }
 
-  const books = result.loading ? [] : result.data.allBooks
+  const books = loading ? [] : data.allBooks
   const genres = books.map(book => book.genres).flat()
   const uniqueGenres = [...new Set(genres)].concat('all genres')
-
+  const handleGenreFilter = (genre) => {
+    setGenreFilter(genre)
+    refetch()
+  }
   return (
     <div>
       <h2>books</h2>
       <div>in genre <b>{genreFilter}</b></div>
       <BookTable books={books} />
       {uniqueGenres.map(genre =>
-        <button key={genre} value={genre} onClick={() => setGenreFilter(genre)}>{genre}</button>
+        <button key={genre} value={genre} onClick={()=>handleGenreFilter(genre)}>{genre}</button>
       )}
     </div>
   )
