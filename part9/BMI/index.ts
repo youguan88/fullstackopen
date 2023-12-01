25ea79cc4a21dca816eb9a0be37a9b56b1aad549
yg <eng.yg@yahoo.com>
2023-12-01 10:39:59 +0800

    9.5 WebBMI

--- part9/BMI/index.ts
+++ part9/BMI/index.ts
@@ -1,13 +1,23 @@
 import express = require('express');
+import calculateBmi from './bmiCalculator';
 const app = express()
 
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!')
 })
 
+app.get('/bmi', (req, res) => {
+    const weight = Number(req.query.weight)
+    const height = Number(req.query.height)
+    const objResponse = {
+        weight, height, bmi: calculateBmi(height, weight)
+    }
+    res.send(objResponse)
+})
+
 const PORT = 3003
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
 
