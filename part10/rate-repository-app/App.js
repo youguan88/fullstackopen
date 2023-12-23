783088fa8201d19587f3b86671e7df4971719593
yg <eng.yg@yahoo.com>
2023-12-23 20:09:18 +0800

    Exercise 10.1: initializing the application

--- part10/rate-repository-app/App.js
+++ part10/rate-repository-app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is working</Text>
+      <Text>This is working finally</Text>
       <StatusBar style="auto" />
     </View>
   );
